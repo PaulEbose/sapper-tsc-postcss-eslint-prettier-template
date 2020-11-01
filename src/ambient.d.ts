e6b2a155fac05ceb5310d71b98aa796945a783ed
Paul Ebose <paulebose@gmail.com>
2020-11-01 02:51:24 +0100

    chore: lint workspace

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -8,32 +8,32 @@
 		<img src="{successkid}">
 	 ```
  */
-declare module "*.gif" {
-	const value: string;
+declare module '*.gif' {
+	const value: string
 	export = value;
 }
 
-declare module "*.jpg" {
-	const value: string;
+declare module '*.jpg' {
+	const value: string
 	export = value;
 }
 
-declare module "*.jpeg" {
-	const value: string;
+declare module '*.jpeg' {
+	const value: string
 	export = value;
 }
 
-declare module "*.png" {
-	const value: string;
+declare module '*.png' {
+	const value: string
 	export = value;
 }
 
-declare module "*.svg" {
-	const value: string;
+declare module '*.svg' {
+	const value: string
 	export = value;
 }
 
-declare module "*.webp" {
-	const value: string;
+declare module '*.webp' {
+	const value: string
 	export = value;
 }
